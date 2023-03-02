d8344ac68bbf9582818eebd3ef2ab4b1c4bf479d
Amos Yu <yua25@mcmaster.ca>
2023-03-02 15:22:50 -0500

    [#2] Use nextPageTokens to recursively retrieve an unlimited number of contacts.

--- src/people.js
+++ src/people.js
@@ -6,30 +6,42 @@ module.exports = async function(auth) {
     google.options({ auth });
     const people = google.people('v1');
 
-    try {
-        const { data } = await people.people.connections.list({
-            personFields: ['names', 'birthdays'],
-            // https://stackoverflow.com/a/60519966/12191708
-            pageSize: 1000,
-            resourceName: 'people/me',
-        });
+    const peopleList = await asyncListAllPeople(people);
 
-        const peopleWithBirthdays = data.connections.filter(person => person.birthdays !== undefined);
-        logger(`§FgFound the birthdays of ${peopleWithBirthdays.length} ${peopleWithBirthdays.length === 1 ? 'person' : 'people'} in your contacts.`);
-        return peopleWithBirthdays.map(person => {
-            const primaryBirthday = person.birthdays.find(birthday => birthday.metadata.primary === true);
-            const primaryName = person.names.find(name => name.metadata.primary === true);
-            return {
-                date: primaryBirthday.date,
-                name: primaryName.displayName,
-            }
-        })
-    } catch (e) {
-        if (e.code === 403) {
-            logger(`§FyFailed to access the People API. Make sure that the §B${package.name} §n§Fyapplication has permission to see and download your contacts.`);
-        } else {
-            logger(`§FyAn unexpected error occurred. Please file a bug report to §B§u${package.bugs.url}.\n§n§Fr${e.stack}`);
+    const peopleWithBirthdays = peopleList.filter(person => person.birthdays !== undefined);
+    logger(`§FgFound the birthdays of ${peopleWithBirthdays.length} ${peopleWithBirthdays.length === 1 ? 'person' : 'people'} in your contacts.`);
+    return peopleWithBirthdays.map(person => {
+        const primaryBirthday = person.birthdays.find(birthday => birthday.metadata.primary === true);
+        const primaryName = person.names.find(name => name.metadata.primary === true);
+        return {
+            date: primaryBirthday.date,
+            name: primaryName.displayName,
         }
-        return [];
+    })
+
+    async function asyncListAllPeople(googlePeople, peopleList = [], pageToken = undefined) {
+        try {
+            const { data } = await googlePeople.people.connections.list({
+                personFields: ['names', 'birthdays'],
+                // https://stackoverflow.com/a/60519966/12191708
+                pageSize: 10,
+                pageToken: pageToken,
+                resourceName: 'people/me',
+            });
+            const { connections, nextPageToken, totalItems } = data;
+            const nextPeopleList = [...peopleList, ...connections];
+    
+            if (nextPageToken && nextPeopleList.length < totalItems) {
+                return asyncListAllPeople(googlePeople, nextPeopleList, nextPageToken);
+            }
+            return nextPeopleList;
+        } catch (e) {
+            if (e.code === 403) {
+                logger(`§FyFailed to access the People API. Make sure that the §B${package.name} §n§Fyapplication has permission to see and download your contacts.`);
+            } else {
+                logger(`§FyAn unexpected error occurred. Please file a bug report to §B§u${package.bugs.url}.\n§n§Fr${e.stack}`);
+            }
+            return [];
+        }
     }
 }
